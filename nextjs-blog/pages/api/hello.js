acad522f14bc23701e3f5be07059691cb42ccde2
Princzes Barnabás <barni.pbs@gmail.com>
2022-01-04 20:41:19 +0100

    hello to citation

--- nextjs-blog/pages/api/hello.js
+++ nextjs-blog/pages/api/hello.js
@@ -1,3 +1,9 @@
+const citations = ["asd", "sasdasdsdaa", "sdfgsdf"];
+
 export default function handler(req, res) {
-  res.status(200).json({ text: "Hello" });
+  res.status(200).json({ text: citationService() });
+}
+
+function citationService() {
+  return citations[Math.floor(Math.random() * citations.length)];
 }
